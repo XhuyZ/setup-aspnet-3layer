15c3c7103957b0e281a78f900f9fa5be3ce67232
XhuyZ <huylovengoc35@gmail.com>
2025-03-06 16:49:50 +0700

    Implement DAL Structure

--- script.sh
+++ script.sh
@@ -8,38 +8,31 @@ cd $PROJECT_NAME
 
 # Initialize a new solution
 dotnet new sln -n API
-# Create the main Web API project
+# Create the main Web API project and 3-layer-architechture
 dotnet new webapi -n API
 dotnet new classlib -n BLL
 dotnet new classlib -n DAL
 dotnet new classlib -n Common
+#Add Project to Solution
 dotnet sln add API/API.csproj
 dotnet sln add BLL/BLL.csproj
 dotnet sln add Common/Common.csproj
 dotnet sln add DAL/DAL.csproj
-# # Create the 3-layer architecture
-# mkdir -p $PROJECT_NAME.BLL
-# mkdir -p $PROJECT_NAME.DAL
-# mkdir -p $PROJECT_NAME.Common
-#
-# # Create Class Library projects
-#
-# # Add them to the solution
-# dotnet sln add $PROJECT_NAME.BLL
-# dotnet sln add $PROJECT_NAME.DAL
-# dotnet sln add $PROJECT_NAME.Common
-#
-# # Add References
-# dotnet add $PROJECT_NAME.BLL reference $PROJECT_NAME.Common
-# dotnet add $PROJECT_NAME.DAL reference $PROJECT_NAME.Common
-# dotnet add $PROJECT_NAME.API reference $PROJECT_NAME.BLL
-# dotnet add $PROJECT_NAME.API reference $PROJECT_NAME.DAL
-#
-# # Create necessary folders
-# mkdir -p $PROJECT_NAME.API/Controllers
-# mkdir -p $PROJECT_NAME.BLL/Services
-# mkdir -p $PROJECT_NAME.DAL/Repositories
-# mkdir -p $PROJECT_NAME.Common/Models
+# Add References
+dotnet add API/API.csproj reference BLL/BLL.csproj
+dotnet add API/API.csproj reference DAL/DAL.csproj
+dotnet add API/API.csproj reference Common/Common.csproj
+dotnet add DAL/DAL.csproj reference Common/Common.csproj
+dotnet add BLL/BLL.csproj reference Common/Common.csproj
+dotnet add BLL/BLL.csproj reference DAL/DAL.csproj
+# DAL (Data Access Layer)
+cd DAL
+mkdir -p Configuration
+mkdir -p Context
+mkdir -p Entities
+mkdir -p Entities
+mkdir -p Migrations
+mkdir -p Repositories/{Interfaces,Implementation}
 #
 # # Create example files
 # echo "namespace $PROJECT_NAME.Common.Models { public class SampleModel { public int Id { get; set; } public string Name { get; set; } } }" > $PROJECT_NAME.Common/Models/SampleModel.cs
